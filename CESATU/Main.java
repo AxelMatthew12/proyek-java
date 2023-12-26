a03b93a61cf0828e818c0658d89b82c34162e788
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-12-26 20:34:13 +0700

    Pending-malas

--- CESATU/Main.java
+++ CESATU/Main.java
@@ -10,14 +10,12 @@ public class Main {
     static void tampilanawal(){
         Scanner scan= new Scanner(System.in);
         System.out.println("------------------------------------------------------");
-        System.out.println("   |                                                | ");
-        System.out.println("------------- SELAMAT DATANG DI CESATU ---------------");
-        System.out.println("==                                                  ==");
-        System.out.println("==                 1.LOGIN-                         ==");
-        System.out.println("==                 2.REGISTER-                      ==");
-        System.out.println("==                 3.KELUAR-                        ==");
+        System.out.println("|------------ SELAMAT DATANG DI CESATU --------------|");
+        System.out.println("=                 1.LOGIN-                           =");
+        System.out.println("=                 2.REGISTER-                        =");
+        System.out.println("=                 3.KELUAR-                          =");
         System.out.println("======================================================");
-        System.out.println("                MASUKKAN PILIHAN                      ");
+        System.out.println("|               MASUKKAN PILIHAN                     |");
         System.out.println("------------------------------------------------------");
         int pilihan= scan.nextInt();
         switch (pilihan) {
@@ -36,35 +34,52 @@ public class Main {
         String idcek="Cesatu";
         int pascek= 123123;
         Scanner scanner= new Scanner(System.in);
-        System.out.println("SILAHKAN MASUKKAN ID : ");
-        String masukid= scanner.nextLine();
-        System.out.println("MASUKKAN PASSWORD ANDA: ");
-        int masukpas= scanner.nextInt();
-        if (masukid.equalsIgnoreCase(idcek)&& masukpas==(pascek)) {
-            System.out.println("\nSTATUS:                                        ");
-            System.out.println("-------------");
-            System.out.println("               <LOGIN BERHASIL>              \n");
-            System.out.println("-----------------------------------------------");
-            System.out.println("                MASUK SEBAGAI ?                ");
-            System.out.println("                 1.MANAJER                     ");
-            System.out.println("                 2.KASIER                      ");
-            System.out.println("-----------------");
-            int pilihankarir= scanner.nextInt();
-            switch (pilihankarir) {
-                case 1:
-                // menumanajer();
-                    break;
-                    case 2:
+
+        boolean berhasillogin= false;
+
+        while (!berhasillogin) {
+            System.out.println(" SILAHKAN MASUKKAN ID: ");
+            String cekid= scanner.nextLine();
+            System.out.println("MASUKKAN PASSWORD: ");
+            int cekpas= scanner.nextInt();
+
+            if (cekid.equalsIgnoreCase(idcek) && cekpas==(pascek)) {
+                System.out.println("\n Status : ");
+                System.out.println("          Berhasil login");
+                System.out.println("------------------------------------------");
+                System.out.println("               MASUK SEBAGAI              ");
+                System.out.println("              1. MANAJER                  ");
+                System.out.println("              2.KASIER                    ");
+                System.out.println("|----------------------------------------|");
+                int pilihkasir= scanner.nextInt();
+
+                switch (pilihkasir) {
+                // case 1:
+                //      menumanajer();
+                //     break;
+                 case 2:
                     menukasier();
                     break;
-                default:
-                System.out.println("SILAHKAN PILIH DARI 1-2");
-                    break;
+                
+                    default:
+                    System.out.println(" Pilih dari 1-2 ! ");
+                    loginmenu();
+                        break;
+                }
+                berhasillogin= true;
+                
+            } else{
+                System.out.println("\n Status: ");
+                System.out.println("        <Login gagal>");
+                System.out.println("Apakah anda ingin mencoba lagi? (y/n)");
+                scanner.nextLine();
+                String cobalagi= scanner.nextLine();
+
+                if (!cobalagi.equalsIgnoreCase("ya")) {
+                    return;
+                    
+                }
             }
-        }
-        else{
-            System.out.println("SISTEM KELUAR");
-             return;
         }
        
 
@@ -84,6 +99,51 @@ public class Main {
         System.out.println("|                        PILIH                               |");
         System.out.println("--------------------------------------------------------------");
         int option= scanner.nextInt();
+        switch (option) {
+            case 1:
+                infomenu();
+                break;
+
+                case 2:
+                transkasimenu();
+
+                break;
+
+                case 3:
+                lakukanpembayaran();
+
+                break;
+
+                case 4:
+                laporanksir();
+
+                break;
+
+                case 5:
+                System.out.println("Anda keluar dari program !");
+        
+            default:
+            System.out.println("SILAHKAN PILIH MENU 1-5");
+                break;
+                
+        }
+    }
+    static void infomenu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("--------------------------------------------");
+        System.out.println("               TAMPILAN MENU                ");
+        
+        
+    }
+    static void transkasimenu(){
+
+
+    }
+    static void lakukanpembayaran(){
+
+    }
+    static void laporanksir(){
+
     }
 }
     
